a552c6e25c3ae845d058dc02f501b3080feaf76b
nurshaerrena <shalenaerrena07@gmail.com>
2021-06-02 15:08:49 +0800

    initial commit
    initial commit

---
+++ db.php
@@ -0,0 +1,10 @@
+<?php // define database connecion variables as constant
+
+define('DB_USER', "root"); // db user
+
+define('DB_PASSWORD',""); //db password
+
+define('DB_DATABASE',"pessdb"); //database name
+
+define('DB_SERVER',"localhost"); //db server
+?>
